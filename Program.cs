4a4470fd7831606699d9b2e2fa55514afef94a4c
Michelle <michellemarcelo791@gmail.com>
2025-11-02 23:42:20 -0300

    termino da atividade

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double nota1, nota2, nota3, media;
+
+Console.WriteLine("média arittmédica de 3 números\n");
+
+Console.Write("digite o primeiro número..:");
+nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("digite o segundo número...:");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("digite o terceiro número..:");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) / 3;
